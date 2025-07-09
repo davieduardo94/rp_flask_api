4cbcfb19d11458f91aef40888878df3f857cdb28
Davi Barbosa <davieduardo94@gmail.com>
2025-07-08 22:14:09 -0400

    configuring database

---
+++ config.py
@@ -0,0 +1,17 @@
+import pathlib
+import connexion
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+basedir = pathlib.Path(__file__).parent.resolve()
+connex_app = connexion.App(__name__, specification_dir=basedir)
+
+#criando o app com connexion
+app = connex_app.app
+
+#caminho do sqlite
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
