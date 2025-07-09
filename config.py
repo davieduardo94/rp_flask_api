0a4d86f19fc74d3d295b3120af95796915606f80
Davi Barbosa <davieduardo94@gmail.com>
2025-07-08 22:18:06 -0400

    update mashmallow variable

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-ma = Marshmallow(app)
+mash = Marshmallow(app)
