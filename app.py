ce444af0f33cc2b250e86701a8f6e3313810943c
Davi Barbosa <davieduardo94@gmail.com>
2025-08-11 21:58:28 -0400

    update app.py for deploy

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import render_template
 import config
 from models import Person
 
-app = config.connex_app #usando o connexion do config.py
+# Connexion App (integrando OpenAPI via swagger.yaml)
+app = config.connex_app
 app.add_api(config.basedir / "swagger.yaml")
 
+# Rota principal para renderizar lista de pessoas
 @app.route("/")
 def home():
     people = Person.query.all()
     return render_template("home.html", people=people)
 
+# Execução local (útil só para testes; ignorado em produção com Gunicorn)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run()
