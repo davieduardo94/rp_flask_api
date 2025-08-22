224a1d9d732812345d05b9df80c17a6fcf06632f
Davi Barbosa <davieduardo94@gmail.com>
2025-08-22 16:54:09 -0400

    revert app.py

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-from flask import render_template, redirect
+from flask import render_template
 import config
 from models import Person
 
-# Connexion App (integrando OpenAPI via swagger.yaml)
-app = config.connex_app
+app = config.connex_app #usando o connexion do config.py
 app.add_api(config.basedir / "swagger.yaml")
 
-# Rota principal para renderizar lista de pessoas
-# @app.route("/")
-# def home():
-#     people = Person.query.all()
-#     return render_template("home.html", people=people)
+@app.route("/")
+def home():
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
-# Flask interno para o WSGI
-flask_app = app.app
-
-# direcionado para UI da api
-@app.route('/')
-def redirect_to_swagger():
-    return redirect('/api/ui')
-
-# Execução local (útil só para testes; ignorado em produção com Gunicorn)
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8000)
+    
