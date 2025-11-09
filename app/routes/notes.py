33168afc07ebfbbacc209955a0c4224109a1fb19
Davi Barbosa <davieduardo94@gmail.com>
2025-11-09 16:45:50 -0400

    creating router for migration

---
+++ app/routes/notes.py
@@ -0,0 +1,59 @@
+# notes.py
+
+from flask import abort, make_response
+
+from app.config import db
+from app.models.models import Note, Person, note_schema
+
+def read_one(note_id):
+    note = Note.query.get(note_id)
+
+    if note is not None:
+        return note_schema.dump(note)
+    else:
+        abort(
+            404, f"Note with ID {note_id} not found"
+        )
+
+
+def create(body):
+    """ CREATE NEW NOTE FOR A PERSON """
+    person_id = body.get("person_id")
+    person = Person.query.get(person_id)
+
+    if person:
+        new_note = note_schema.load(body, session = db.session)
+        person.notes.append(new_note)
+        db.session.commit()
+        return note_schema.dump(new_note), 201
+    else:
+        abort(
+            404,
+            f"Person not found for ID: {person_id}"
+        )
+
+
+def update(note_id, body):
+    """ UPDATE NOTE BY ID """
+    existing_note = Note.query.get(note_id)
+
+    if existing_note:
+        update_note = note_schema.load(body, session=db.session)
+        existing_note.content = update_note.content
+        db.session.merge(existing_note)
+        db.session.commit()
+        return note_schema.dump(existing_note), 201
+    else:
+        abort(404, f"Note with ID {note_id} not found.")
+
+
+def delete(note_id):
+    """ DELETE NOTE BY ID """
+    existing_note = Note.query.get(note_id)
+    
+    if existing_note:
+        db.session.delete(existing_note)
+        db.session.commit()
+        return make_response(f"{note_id} successfully deleted.", 204)
+    else:
+        abort(404,f"Note with ID {note_id} not found")
