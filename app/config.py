58769b89c6b5eee0e5f3ec8e7af7137944999c47
Davi Barbosa <davieduardo94@gmail.com>
2025-11-18 21:48:56 -0400

    created config.py

--- app/config.py
+++ app/config.py
@@ -1,17 +1,12 @@
+# app/config.py
 import pathlib
-import connexion
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 
-basedir = pathlib.Path(__file__).parent.resolve()
-connex_app = connexion.App(__name__, specification_dir=basedir)
+basedir = pathlib.Path(__file__).resolve().parent.parent
 
-#criando o app com connexion
-app = connex_app.app
+class Config:
+    MONGODB_SETTINGS = {
+        "db": "pessoas_db",
+        "host": "mongodb://localhost:27017/pessoas_db"
+    }
 
-#caminho do sqlite
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-mash = Marshmallow(app)
+    JSON_SORT_KEYS = False
