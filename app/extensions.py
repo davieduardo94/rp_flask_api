e1e737298e95aa86b1a1e6194177b56d82d38e31
Davi Barbosa <davieduardo94@gmail.com>
2025-11-18 21:49:08 -0400

    created extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,2 +1,6 @@
-from flask_mongoengine import MongoEngine
-db = MongoEngine()
+# app/extensions.py
+from flask_pymongo import PyMongo
+from flask_marshmallow import Marshmallow
+
+mongo = PyMongo()
+mash = Marshmallow()
