638e90f1bba380d3f33baddc373fe6210f00bd3d
Davi Barbosa <davieduardo94@gmail.com>
2025-11-23 15:56:46 -0400

    update person model

--- app/models/person.py
+++ app/models/person.py
@@ -1,16 +1,14 @@
 # app/models/pessoa.py
-from app.extensions import db, mash
-from datetime import datetime
+from app.config import mash
+from marshmallow import Schema, fields
 
-class Person(db.Document):
-    full_name = db.StringField(required=True, max_length=32)
-    age = db.IntField(required=True, min_value=0, max_value=150)
-    phone_number = db.ListField(db.StringField())
-    created_date = db.DateTimeField(default=datetime.now)
+class PersonSchema(Schema):
+    _id = fields.Str()   # _id convertido STR
+    full_name = fields.Str(required=True)
+    age = fields.Int(required=True)
+    phone_number = fields.List(fields.Str())
+    created_date = fields.DateTime()
 
-class PersonSchema(mash.Schema):
-    class Meta:
-        fields = ("id", "full_name", "age", "created_date")
 
 people_schema = PersonSchema() # 1 pessoa
 person_schema = PersonSchema(many=True) # varias pessoas
