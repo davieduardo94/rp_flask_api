3b3b04cdebb0d451ad819286f93cc09bf859ca4e
Davi Barbosa <davieduardo94@gmail.com>
2025-11-23 23:00:04 -0400

    feat: call connexion and mongoclient from extension flie

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,38 @@
 # app/__init__.py
+
 import pathlib
 import connexion
-from pathlib import Path
 from flask import render_template
-from app.config import Config
-from app.extensions import db, mash
-from routes.person_routes import person_bp
+from pymongo import MongoClient
+from .config import Config
+from .extensions import mash, mongo_client, mongo_db   # marshmallow inicializado aqui
+from .routes.people_routes import people_bp
 
+# Caminho base
 basedir = pathlib.Path(__file__).parent.resolve()
 
-# Cria app Connexion e adiciona o Swagger file
+# 1) Criar o Connexion App
 connex_app = connexion.App(__name__, specification_dir=str(basedir))
-connex_app.add_api("swagger.yaml")
+connex_app.add_api("swagger.yaml")  # importa o swagger
 
-# Flask interno
+# Flask "interno" do Connexion
 app = connex_app.app
+
+# Carregar configurações do config.py
 app.config.from_object(Config)
 
-# Inicializa extensões
-db.init_app(app)
+# Disponibilizar para o projeto inteiro
+app.mongo_client = mongo_client
+app.mongo_db = mongo_db
+
+# 2) Inicializar Marshmallow
 mash.init_app(app)
 
-# Registra blueprint
-app.register_blueprint(person_bp, url_prefix="/pessoas")
+# 3) Registrar Blueprints
+app.register_blueprint(people_bp, url_prefix="/people")
 
-# Página inicial (home.html)
+# 4) Rota inicial (opcional)
 @app.route("/")
 def home():
-    pessoas = db.connection["pessoas_db"]["pessoa"].find()
-    return render_template("home.html", people=pessoas)
+    peopple = list(mongo_db.people.find())  # nome da coleção
+    return render_template("home.html", people=peopple)
