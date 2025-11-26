5d1d845cddb92d79496661f85be14c78acf71d03
Davi Barbosa <davieduardo94@gmail.com>
2025-11-25 23:00:13 -0400

    create: read one person function

--- app/routes/people_routes.py
+++ app/routes/people_routes.py
@@ -1,7 +1,7 @@
 # app/routes/pessoas_routes.py
-from flask import Blueprint, current_app
+from flask import Blueprint, abort, current_app
 from app.extensions import mongo_client, mongo_db
-from app.models.person import people_schema
+from app.models.person import people_schema, person_schema
 
 people_bp = Blueprint("people", __name__)
 
@@ -20,3 +20,13 @@ def list_people():
 
     return people_schema.dump(people_list)
 
+def read_person(full_name):
+    db = current_app.mongo_db
+    person = db.people.find_one({"full_name" : full_name}, {"_id" : 0, "created_date": 0})
+
+    if person:
+        return person_schema.dump(person)
+    else:
+        abort(
+            404, f"Pessoa com o nome {full_name} não encontrada!"
+        )
