d20bbe80768257435396f6fefda6a2869d0f6d3d
Davi Barbosa <davieduardo94@gmail.com>
2025-12-02 21:49:35 -0400

    update: host url

--- migrations/app.py
+++ migrations/app.py
@@ -1,6 +1,6 @@
 from flask import render_template
-import app.config as config
-from app.models.models import Person
+import config as config
+from models import Person
 
 app = config.connex_app #usando o connexion do config.py
 app.add_api(config.basedir / "swagger.yaml")
@@ -11,5 +11,5 @@ def home():
     return render_template("home.html", people=people)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="localhost", port=8000)
     
